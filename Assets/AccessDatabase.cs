e7bf5d78d96389ca3ed361d3f9ff83a3d9ce82aa
Russell <russell.ayres.90@gmail.com>
2019-08-01 10:17:18 +1000

    First Push

---
+++ Assets/AccessDatabase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using UnityEngine;
+
+using Firebase.Unity.Editor;
+
+public class AccessDatabase : MonoBehaviour
+{
+
+    public string testing;
+    // Start is called before the first frame update
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+
+// Handle initialization of the necessary firebase modules:
+    public void InitializeFirebase() {
+        Debug.Log("Setting up Firebase Auth");
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+// Track state changes of the auth object.
+    void AuthStateChanged(object sender, System.EventArgs eventArgs) {
+        if (auth.CurrentUser != user) {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null) {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn) {
+                Debug.Log("Signed in " + user.UserId);
+            }
+        }
+    }
+
+    private void Start()
+    {
+
+        
+    }
+
+    public void RunAthing()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://test-32217.firebaseio.com/");
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
+        string json = JsonUtility.ToJson(testing);
+
+        reference.Child("users").Child(user.UserId).SetValueAsync(json);
+    }
+    
+    
+}
---
+++ Assets/FirebaseAuth.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using UnityEngine;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class FirebaseAuth : MonoBehaviour
+{
+    public string test;
+    public string email = "1011185@student.sae.edu.au";
+    
+    
+    public string password = "123456";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Signup()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser user = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                user.DisplayName, user.UserId);
+        });
+    }
+
+    public void SignIn()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    
+
+}
+
